04a4c03b0e711b3baeb2c0fe67941b1108c3e0b0
Meowu <474384902@qq.com>
2018-01-28 23:03:36 +0800

    start to write components

---
+++ src/components/Chip/VChip.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'v-chip',
+  methods: {
+    getContent(h) {
+      const children = [this.$slots.default]
+      const style = {
+        height: '28px',
+        zIndex: 1,
+        fontSize: '14px',
+        // verticalAlign: 'middle',
+        padding: '0 12px',
+        display: 'inline-flex',
+        alignItems: 'center',
+        lineHeight: '28px'
+      }
+      return h('span', {
+        'class': 'chip__content',
+        style: style,
+      }, children)
+    }
+  },
+  render(h) {
+    const style = {
+      display: 'inline-flex',
+      alignItems: 'center',
+      borderRadius: '4px',
+      backgroundColor: 'rgba(0, 0, 0, .12)',
+      margin: '4px',
+      verticalAlign: 'middle',
+      outline: 'none'
+    }
+    const data = {
+      staticClass: 'chip',
+      style: style,
+      on: this.$listeners
+    }
+    return h('span', data, [this.getContent(h)])
+  }
+}
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,10 +1,21 @@
+<script>
+import VChip from '../components/Chip/VChip'
+export default {
+  name: 'Hello',
+  components: {
+    VChip
+  }
+}
+</script>
+
 <template>
   <div id='hello'>
     This is my first Vue SSR app.
+    <v-chip>外卖</v-chip>
   </div>
 </template>
 <style lang="stylus" scoped>
 #hello
-  font-size .5rem
+  font-size .2rem
   padding 0 1rem
 </style>
