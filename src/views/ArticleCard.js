d1f2ee0ceb77361c642fc65a4e828a4d515c6052
Meowu <474384902@qq.com>
2018-01-31 23:59:53 +0800

    change card

--- src/views/ArticleCard.js
+++ src/views/ArticleCard.js
@@ -3,24 +3,27 @@ export default {
   name: "v-article-card",
   data() {
     return {
-      text: 'Listen to your favorite artists and albums whenever and wherever, online and offline.',
-      title: "Super Model",
+      text: 'include 和 exclude 属性允许组件有条件地缓存。二者都可以用逗号分隔字符串、正则表达式或一个数组来表示。',
+      title: "Vue 的实现原理和源码分析",
       tags: ['Python', 'Node.js', 'Go', 'Rust']
     }
   },
   methods: {
     genTitle(h, title) {
       const style = {
-        fontSize: '30px',
-        fontWeight: 400,
+        fontSize: '15px',
+        fontWeight: 600,
         letterSpacing: '-.02em',
         lineHeight: '40px',
-        padding: '.4em'
+        padding: '.2em .8em',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
       }
       return h('h3', {
         staticClass: 'card-title',
         style: style
-      }, title)
+      }, [h('span', title), h('span',{style:{fontSize: '12px', fontWeight: '200', color: 'rgba(0,0,0,.5)', marginRight: '10px'}}, '2018-02-30')])
     },
     genText(h, text) {
       const style = {
@@ -50,6 +53,7 @@ export default {
     const tags = this.genTags(h, this.tags)
     const text = this.genText(h, this.text)
     const title = this.genTitle(h, this.title)
+    // const meta = h('')
     return h('div', {
       staticClass: 'card',
       style: {
