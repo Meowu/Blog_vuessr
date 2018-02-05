d012d4f17707746c6a0615b8199d89ddf7c27daf
Meowu <474384902@qq.com>
2018-02-05 22:55:15 +0800

    add aboout page and archive pages

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ const createList = id => () => import('../views/CreateListView').then(m => m.def
 // 类似于 createApp，我们也需要给每个请求一个新的 router 实例，所以文件导出一个 createRouter 函数:
 const Home = () => import('../views/Home.js')
 const ArticlePage = () => import('../views/ArticlePage.js')
+const ArchivePage = () => import('../views/ArchivePage.js')
+const AboutPage = () => import('../views/AboutPage.js')
 
 export function createRouter() {
   return new Router({
@@ -18,24 +20,16 @@ export function createRouter() {
         component: Home
       },
       {
-        path: '/good',
-        component: createList('good')
-      },
-      {
-        path: '/share',
-        component: createList('share')
-      },
-      {
-        path: '/ask',
-        component: createList('ask')
-      },
-      {
-        path: '/job',
-        component: createList('job')
+        path: '/archives',
+        component: ArchivePage
       },
       {
         path: '/articles/:id',
         component: ArticlePage
+      },
+      {
+        path: '/aboutme',
+        component: AboutPage
       },
       // {
       //   path: '*',
---
+++ src/views/ArchivePage.js
@@ -0,0 +1,3 @@
+export default {
+  name: "archive-page"
+}
